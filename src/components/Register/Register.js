603081151fa2a1a5ca22e752bb26b104a5779f04
martin2117 <martin2117@azet.sk>
2020-02-23 23:46:53 +0100

    Remove unused variable

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -43,8 +43,6 @@ onSubmitRegister = () => {
 
 
 render() {
-
- const { onRouteChange } = this.props
     return (
         <article className="br5 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
